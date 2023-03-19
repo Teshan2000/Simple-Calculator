ef37f4870523d131c37d929f6d39ebf39de24480
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-03-20 01:15:06 +0530

    Basic Calculating Functions
    Calculating functions were added to the calculator program

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,3 +1,16 @@
+def add(x,y):
+    return x + y
+def subtract(x,y):
+    return x - y
+def multiply(x,y):
+    return x * y
+def divide(x,y):
+    return x / y
+def Power(x,y):
+    return x ** y
+def Remainder(x,y):
+    return x % y
+
 Num1 = int(input("Enter your first number: "))
 Num2 = int(input("Enter your second number: "))
 
@@ -15,3 +28,11 @@ if choice == '*':
     
 if choice == '/':
     print(Num1 + " / " +  Num2 + " = ")
+
+if choice == '^':
+    print(Num1 + " ^ " +  Num2 + " = ")
+
+if choice == '%':
+    print(Num1 + " % " +  Num2 + " = ")
+
+    
