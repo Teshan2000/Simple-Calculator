bf355a6a52134d09abadec1f9260e9574a76227c
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-03-21 22:45:55 +0530

    Functions
    calculating functions were updated

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -14,26 +14,26 @@ def Remainder(x,y):
 Num1 = int(input("Enter your first number: "))
 Num2 = int(input("Enter your second number: "))
 
-choice = str(input("Enter your choice: /n"))
 print("| + | - | * | / | % | ^ |")
+choice = str(input("Enter your choice: \n"))
 
 if choice == '+':
-    print(Num1 + " + " +  Num2 + " = ")
+    print(Num1, " + ", Num2, " = ", add(Num1,Num2))
 
 elif choice == '-':
-    print(Num1 + " - " + Num2 + " = ")
+    print(Num1, " - ", Num2, " = ", subtract(Num1,Num2))
     
 elif choice == '*':
-    print(Num1 + " * " +  Num2 + " = ")
+    print(Num1 + " * " +  Num2 + " = ", multiply(Num1,Num2))
     
 elif choice == '/':
-    print(Num1 + " / " +  Num2 + " = ")
+    print(Num1 + " / " +  Num2 + " = ", divide(Num1,Num2))
 
 elif choice == '^':
-    print(Num1 + " ^ " +  Num2 + " = ")
+    print(Num1 + " ^ " +  Num2 + " = ", Power(Num1,Num2))
 
 elif choice == '%':
-    print(Num1 + " % " +  Num2 + " = ")
+    print(Num1 + " % " +  Num2 + " = ", Remainder(Num1,Num2))
 
 else:
     print("Something went wrong")
