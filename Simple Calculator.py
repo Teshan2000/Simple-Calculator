9ee6b6c08d6587df29d37461331a2f49c690aeec
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-03-31 00:16:26 +0530

    FIxed operations
    Some issues were fixed and all functions were written

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -20,54 +20,61 @@ def Remainder(x,y):
     return x % y
 
 def select_option(choice):
-    if choice == '#':
+    if (choice=='#'):
         print("~Terminated~")
         exit()
+
+    elif (choice=='$'):
+        exit()
+
+    elif (choice in '+','-','*','/','^','%'):
+        while (True):
+            
+            Num1 = int(input("Enter your first number: "))
+            print(Num1)
+
+            Num2 = int(input("Enter your second number: "))
+            print(Num2)
+
+            
+            if choice == '+':
+                print(Num1, " + ", Num2, " = ", add(Num1,Num2))
+
+            elif choice == '-':
+                print(Num1, " - ", Num2, " = ", subtract(Num1,Num2))
+                    
+            elif choice == '*':
+                print(Num1, " * ", Num2, " = ", multiply(Num1,Num2))
+                    
+            elif choice == '/':
+                print(Num1, " / ", Num2, " = ", divide(Num1,Num2))
+
+            elif choice == '^':
+                print(Num1, " ^ ", Num2, " = ", Power(Num1,Num2))
+
+            elif choice == '%':
+                print(Num1, " % ", Num2, " = ", Remainder(Num1,Num2))
+
+            else:
+                print("Something went wrong")
+
+while(True):
+
+    print("Select operation:")
+    print("1.Add      : + ")
+    print("2.Subtract : - ")
+    print("3.Multiply : * ")
+    print("4.Divide   : / ")
+    print("5.Power    : ^ ")
+    print("6.Remainder: % ")
+    print("7.Terminate: # ")
+    print("8.Reset    : $ ")
+
+    choice = str(input("Enter your choice: \n"))
     
-    if choice == '$':
-        return -1
 
 
-Num1 = int(input("Enter your first number: "))
-Num2 = int(input("Enter your second number: "))
+print("----------------------------------------------")
 
-        
-print("Select operation:")
-print("1.Add      : + ")
-print("2.Subtract : - ")
-print("3.Multiply : * ")
-print("4.Divide   : / ")
-print("5.Power    : ^ ")
-print("6.Remainder: % ")
-print("7.Terminate: # ")
-print("8.Reset    : $ ")
-
-choice = str(input("Enter your choice: \n"))
-
-#ans = "Your answer is {}"
-#print(ans.format())
-     
-while (Num1 or Num2 != 0):
-
-    if choice == '+':
-        print(Num1, " + ", Num2, " = ", add(Num1,Num2))
-
-    elif choice == '-':
-        print(Num1, " - ", Num2, " = ", subtract(Num1,Num2))
-            
-    elif choice == '*':
-        print(Num1, " * ", Num2, " = ", multiply(Num1,Num2))
-            
-    elif choice == '/':
-        print(Num1, " / ", Num2, " = ", divide(Num1,Num2))
-
-    elif choice == '^':
-        print(Num1, " ^ ", Num2, " = ", Power(Num1,Num2))
-
-    elif choice == '%':
-        print(Num1, " % ", Num2, " = ", Remainder(Num1,Num2))
-
-    else:
-        print("Something went wrong")
 
    
